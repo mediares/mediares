50aeb8e1c5bedbebd15e47e4f51ed8a57185525a
Labrys of Knossos <Labrys.git@gmail.com>
2019-07-17 22:15:52 -0400

    Add country name check to tvmaze country parsing

---
+++ tests/test_tvmaze/test_parsers/test_country.py
@@ -0,0 +1,42 @@
+import pycountry.db
+import datetime
+
+import pytest
+
+import mediares.maze.parsers
+
+
+@pytest.mark.parametrize(
+    'data', [
+        None,
+        {
+            'code': 'US',
+            'name': 'United States',
+            'timezone': 'America/New_York',
+        },
+    ],
+)
+def test_parse_network(data):
+    known_keys = {
+        'code',
+        'name',
+        'timezone',
+    }
+    # Make sure there were no unknown keys in the data to be parsed
+    assert data is None or not known_keys.symmetric_difference(data.keys())
+
+    expected_keys = {
+        'country',
+        'timezone',
+    }
+    parsed = mediares.maze.parsers.parse_country(data)
+    # Make sure all expected keys exist in parsed result
+    assert not expected_keys.symmetric_difference(parsed.keys())
+
+    country = parsed['country']
+    assert country is None or isinstance(country, pycountry.db.Data)
+
+    timezone = parsed['timezone']
+    assert timezone is None or isinstance(timezone, datetime.tzinfo)
+
+    assert (country is None and timezone is None) or (country and timezone)
