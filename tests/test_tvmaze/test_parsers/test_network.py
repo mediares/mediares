3fcac276404cc8c79f3f852d827365911246d6ef
Labrys of Knossos <Labrys.git@gmail.com>
2019-07-17 21:17:27 -0400

    Fix tvmaze network parser tests

--- tests/test_tvmaze/test_parsers/test_network.py
+++ tests/test_tvmaze/test_parsers/test_network.py
@@ -1,3 +1,6 @@
+import pycountry.db
+import datetime
+
 import pytest
 
 import mediares.maze.parsers
@@ -11,12 +14,20 @@ import mediares.maze.parsers
                 'name': 'United States',
                 'timezone': 'America/New_York',
             },
-         'id': 1,
-         'name': 'NBC',
+            'id': 1,
+            'name': 'NBC',
         },
     ],
 )
 def test_parse_network(data):
+    known_keys = {
+        'country',
+        'id',
+        'name',
+    }
+    # Make sure there were no unknown keys in the data to be parsed
+    assert not known_keys.symmetric_difference(data.keys())
+
     expected_keys = {
         'country',
         'maze_id',
@@ -24,6 +35,15 @@ def test_parse_network(data):
         'timezone',
     }
     parsed = mediares.maze.parsers.parse_network(data)
-
     # Make sure all expected keys exist in parsed result
     assert not expected_keys.symmetric_difference(parsed.keys())
+
+    assert isinstance(parsed['maze_id'], int)
+    assert parsed['maze_id'] > 0
+
+    assert isinstance(parsed['name'], str)
+    assert parsed['name']
+
+    assert isinstance(parsed['country'], pycountry.db.Data)
+
+    assert isinstance(parsed['timezone'], datetime.tzinfo)
